9043271cca4b48480c99a912e873f11a4dbad401
Danila Kozlyakovsky <danik2003globin@gmail.com>
2023-07-29 17:15:57 +0300

    first step to architecture(logger, controller, bad di, test ts decorators)

---
+++ src/errors/exeption.filter.interface.ts
@@ -0,0 +1,5 @@
+import { NextFunction, Request, Response, response } from "express";
+
+export interface IExeptionFilter {
+  catch: (err: Error, req: Request, res: Response, next: NextFunction) => void;
+}
